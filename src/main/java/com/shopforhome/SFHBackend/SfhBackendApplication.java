9a4141fc8843e967fab89fb81e01ae8cb5482772
Slackjaw1431 <55801287+Slackjaw1431@users.noreply.github.com>
2023-11-30 04:20:20 -0500

    Initial commit

---
+++ src/main/java/com/shopforhome/SFHBackend/SfhBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shopforhome.SFHBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfhBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfhBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopforhome/SFHBackend/SfhBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopforhome.SFHBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfhBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
